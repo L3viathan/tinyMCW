0cd7d62af9c2e922ec92821adab9202248ec2f7c
Jonathan Oberländer <github@l3vi.de>
2015-10-31 03:27:47 +0100

    Bugfix

--- plugins/tp2first.py
+++ plugins/tp2first.py
@@ -1,6 +1,6 @@
 def cond_tp(player, mc):
     if len(mc.players) == 1:
-        old = list(players)[0]
+        old = list(mc.players)[0]
         mc.send("tp {} {}".format(player, old))
 
 _hooks = {'join':cond_tp}
