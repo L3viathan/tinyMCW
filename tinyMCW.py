6f44c0806d82d7c9b9b8bf2ef168ef183d87ae77
L3viathan <git@l3vi.de>
2015-10-31 03:07:48 +0100

    Start Minecraft through the Minecraft object. Less messy, everything.

--- tinyMCW.py
+++ tinyMCW.py
@@ -5,6 +5,10 @@ from time import sleep
 from glob import glob
 from collections import defaultdict
 from importlib import import_module
+linere = re.compile(r"\[(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})\] \[(?P<thread>[^/]+)\/(?P<level>\w+)\]: (?P<message>.*)")
+chatre = re.compile(r"<(?P<player>\w+)> (?P<message>.*)")
+sayre = re.compile(r"\[\w+\] (?P<message>.*)")
+joinleftre = re.compile(r"(?P<player>\w+) (?P<change>joined|left) the game")
 
 hooks = defaultdict(list)
 
@@ -13,45 +17,18 @@ for plugin in glob("plugins/*.py"):
     p = import_module(plugin.replace("/",".").replace(".py",""))
     for hook in p._hooks:
         hooks[hook].append(p._hooks[hook])
+
 class Minecraft():
-    def __init__(self, proc)
-        self.proc = proc
+    '''Global State Object.'''
+    def __init__(self, filename="minecraft_server.jar"):
+        self.proc = pexpect.spawn("java -jar {}".format(filename))
         self.players = set()
     def send(self, message):
         print(">",message,file=sys.stderr)
-        self.mc.sendline(message)
+        self.proc.sendline(message)
 
-mcproc = pexpect.spawn("java -jar minecraft_server.jar")
-mc = Minecraft(mcproc)
+mc = Minecraft()
 
-
-
-def handle_chat(message, player, fields, mc):
-    for hook in hooks.get('chat',()):
-        hook(message=message, player=player, mc=mc)
-    if message[0] == "!":
-        for hook in hooks.get('command',()):
-            command = message[1:].split()
-            hook(command=command[0], args=command[1:], player=player, mc=mc)
-
-def handle_say(message, fields, mc):
-    for hook in hooks.get('say',()):
-        hook(message=message, mc=mc)
-
-def handle_join(player, fields, mc):
-    for hook in hooks.get('join',()):
-        hook(player=player, mc=mc)
-    players.add(player)
-
-def handle_leave(player, fields, mc):
-    for hook in hooks.get('leave',()):
-        hook(player=player, mc=mc)
-    players.remove(player)
-
-linere = re.compile(r"\[(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})\] \[(?P<thread>[^/]+)\/(?P<level>\w+)\]: (?P<message>.*)")
-chatre = re.compile(r"<(?P<player>\w+)> (?P<message>.*)")
-sayre = re.compile(r"\[\w+\] (?P<message>.*)")
-joinleftre = re.compile(r"(?P<player>\w+) (?P<change>joined|left) the game")
 def handle(line, mc):
     '''Handles line, calls hooks, etc.'''
     match = linere.match(line)
@@ -64,20 +41,31 @@ def handle(line, mc):
         match = chatre.match(fields['message'])
         if match:
             chat = match.groupdict()
-            return handle_chat(message=chat['message'].strip(), player=chat['player'], fields=fields, mc=mc)
+            if chat['message'][0] == "!":
+                for hook in hooks.get('command',()):
+                    command = chat['message'][1:].split()
+                    hook(command=command[0], args=command[1:], player=chat['player'], mc=mc)
+            else:
+                for hook in hooks.get('chat',()):
+                    hook(message=chat['message'], player=chat['player'], mc=mc)
 
         match = sayre.match(fields['message'])
         if match:
             say = match.groupdict()
-            return handle_say(message=say['message'], fields=fields, mc=mc)
+            for hook in hooks.get('say',()):
+                hook(message=say['message'], mc=mc)
 
         match = joinleftre.match(fields['message'])
         if match:
             joinleft = match.groupdict()
             if joinleft['change'] == "joined":
-                return handle_join(player=joinleft['player'], fields=fields, mc=mc)
+                for hook in hooks.get('join',()):
+                    hook(player=joinleft['player'], mc=mc)
+                mc.players.add(joinleft['player'])
             else:
-                return handle_leave(player=joinleft['player'], fields=fields, mc=mc)
+                for hook in hooks.get('leave',()):
+                    hook(player=joinleft['player'], mc=mc)
+                mc.players.remove(joinleft['player'])
 
 while True:
     try:
